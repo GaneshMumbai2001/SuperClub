894389f78d3a2ce8971b360d8d5da4ebcef7896c
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-21 02:51:29 +0530

    Adjusted the btn style

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,26 +7,28 @@ import About from './About';
 const HeroSection = () => {
   return (
     <section>
-      <div className='grid grid-cols-1 lg:grid-cols-12 bg-[#000000]'>
-        <div className='col-span-7 place-self-center text-center xl:text-center'>
-      <h1 className='text-white mb-4 md:lg:text-10xl xl:lg:text-8xl font-extrabold'>
-       <span className='text-transparent bg-clip-text  bg-gradient-to-r from-purple-400 to-pink-400 md:text-center'> 
-       Hello! Welcome to the SuperClub{""}
+      <div className=' bg-[#000000]'>
+      <div className='place-self-center text-center xl:text-center'>
+      <div className='flex justify-between mx-10'>
+      <div className='text-white mb-4 lg:md:text-10xl xl:lg:text-8xl font-extrabold'>
+       <span className='pl-32 text-transparent pd-24   text-center md:bg-clip-text  bg-gradient-to-r from-blue-400 to-blue-400  justifyContent:center alignItems: center d-flex '> 
+       Hello <br /> Welcome to the SuperClub{""}
        </span>
- 
-        </h1>
-      <p className='text-[#ADB7BE] md:text-base lg:text-5-xl mb-10 xl:lg:text-5xl'>
+        </div>
+        <div className='mt-10'>
+          <ConnectButton  />
+        </div>
+        </div>
+      <p className='text-[#ADB7BE] md:text-base lg:text-5-xl mb-10 xl:lg:text-5xl mt-52'>
       Speak Freely, Earn Seamlessly.
        Your Voice , Your Rewards!
         </p>
         <div>
-          <button className='px-12 py-3.5 w-full xl:lg:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-200 via-sky-500 to-pink-500 hover:bg-slate-200 text-white '>Earn</button>
-          
-          < button className='px-5 py-3.5 w-full xl:lg:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-200 via-sky-500 to-pink-500 hover:bg-slate-200 text-white '>ConnectButton</button>
+          <button className='md:px-10 py-1.5  font-bold rounded-lg  mr-4 bg-gradient-to-br from-blue-400 to-blue-600 hover:bg-slate-200 text-white items-center'>Earn</button> 
+           
         </div>
         </div>
-   
-          <About/>
+
         </div>
     </section>
   );
