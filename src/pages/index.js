70edb7b3f7728d622d219fd8f8c4e145d60c323c
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-20 23:44:20 +0530

    Added the get started

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,16 @@ const HeroSection = () => {
         <div className='col-span-7 place-self-center text-center sm:text-center'>
       <h1 className='text-white mb-4 sm:text-10xl lg:text-8xl font-extrabold'>
        <span className='text-transparent bg-clip-text text-center bg-gradient-to-r from-purple-400 to-pink-400 text-center'> 
-       Hello! Welcome to the{""}
+       Hello! Welcome to the SuperClub{""}
        </span>
  
         </h1>
       <p className='text-[#ADB7BE] text-base sm:text-lg mb-10 lg:text-5xl'>
-       SuperClub
+      Speak Freely, Earn Seamlessly.
+       Your Voice , Your Rewards!
         </p>
         <div>
-          <button className='px-7 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-200 via-sky-500 to-pink-500 hover:bg-slate-200 text-white '>Hi</button>
+          <button className='px-4 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-200 via-sky-500 to-pink-500 hover:bg-slate-200 text-white '>Hi</button>
           <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:big-slate-800 hover:bg-slate-800 text-white mt-3'>
             <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Connect Wallet </span>
             </button>
